9e4193aaf172a3c369cb93c3243c63f8dfc1fbe4
goreleaserbot <goreleaser@carlosbecker.com>
2020-02-23 22:53:54 +0000

    Brew formula update for go-pwa-server version v0.0.1

--- Formula/go-pwa-server.rb
+++ Formula/go-pwa-server.rb
@@ -2,24 +2,24 @@
 class GoPwaServer < Formula
   desc "🚀 Fast static server for your PWA applications"
   homepage "https://github.com/brunoluiz/go-pwa-server"
-  version "0.0.0"
+  version "0.0.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/brunoluiz/go-pwa-server/releases/download/v0.0.0/go-pwa-server_Darwin_x86_64.tar.gz"
-    sha256 "0231339a39feeafe5049b25647ca39dd3f520af4b662e1c31f94cd3fa660312d"
+    url "https://github.com/brunoluiz/go-pwa-server/releases/download/v0.0.1/go-pwa-server_0.0.1_darwin_amd64.tar.gz"
+    sha256 "f84632d75c036e07b646863e28a8d9f3c3c46717700d97bfda33981f5dbca1fa"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/brunoluiz/go-pwa-server/releases/download/v0.0.0/go-pwa-server_Linux_x86_64.tar.gz"
-      sha256 "8fe54e39c9d9923bcf007ecb458626f01ed97c455a71c4cd277779c0c358b2b0"
+      url "https://github.com/brunoluiz/go-pwa-server/releases/download/v0.0.1/go-pwa-server_0.0.1_linux_amd64.tar.gz"
+      sha256 "f4d3040106e51b75d76e06da76bcfa5e68ee70cb99309b9004beb41bf5aa80f9"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/brunoluiz/go-pwa-server/releases/download/v0.0.0/go-pwa-server_Linux_arm64.tar.gz"
-        sha256 "ac614d8abefb0f80f340ee74b159532710a2b0bd55346a2ba978ee9e00c94a3c"
+        url "https://github.com/brunoluiz/go-pwa-server/releases/download/v0.0.1/go-pwa-server_0.0.1_linux_arm64.tar.gz"
+        sha256 "c3b7211eb845772a29d69f811d5205cdf812e5eaf80f3aea60013ff1ee77f067"
       else
-        url "https://github.com/brunoluiz/go-pwa-server/releases/download/v0.0.0/go-pwa-server_Linux_armv6.tar.gz"
-        sha256 "b25fa2c33ef24c63a42e74fb8f60157659124c160f755156b21facb8262aeaa7"
+        url "https://github.com/brunoluiz/go-pwa-server/releases/download/v0.0.1/go-pwa-server_0.0.1_linux_armv6.tar.gz"
+        sha256 "6ee9b7b2ede1e232dbeb54ef515a170e3af9651f996be2009087eccd18350e6d"
       end
     end
   end
