72f2794540875103af115b5e95670fad247130cd
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-05 19:09:45 +0000

    Brew formula update for jornada version v0.3.0

--- Formula/jornada.rb
+++ Formula/jornada.rb
@@ -5,16 +5,16 @@
 class Jornada < Formula
   desc ""
   homepage ""
-  version "0.2.0"
+  version "0.3.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/brunoluiz/jornada/releases/download/v0.2.0/jornada_0.2.0_darwin_amd64.tar.gz"
-    sha256 "1097ac2bd43e806a4e5212837837c996ca2655e99c1246f051fa25fe3f366d9f"
+    url "https://github.com/brunoluiz/jornada/releases/download/v0.3.0/jornada_0.3.0_darwin_amd64.tar.gz"
+    sha256 "24f224746ffa2f03716a1b4e0fb8068282f37921a937c1a51ed5128688c3bd52"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/brunoluiz/jornada/releases/download/v0.2.0/jornada_0.2.0_linux_amd64.tar.gz"
-    sha256 "6db59dc8f24d4c08e02d3a1efc74c2ab2e00a71e5d52bec7e38fcb3a6422a35b"
+    url "https://github.com/brunoluiz/jornada/releases/download/v0.3.0/jornada_0.3.0_linux_amd64.tar.gz"
+    sha256 "be957ccf4bb584b39483dd26899bf54afb74a068bf1a38d56ace66bb7f1d4cba"
   end
 
   def install
